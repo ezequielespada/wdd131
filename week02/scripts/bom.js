c58e915413972e94d686fc8c372f836fd5a2ecfb
Ezequiel Espada <e.h.espada@gmail.com>
2024-09-25 22:27:12 -0300

    Completing JS

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,28 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('li')
-const item = document.createElement('li');
-const deleteButton = document.createElement('button');
-item.textContent = input.value;
-deleteButton.textContent = 'X';
-item.append(deleteButton);
-list.append(item);
+const list = document.querySelector('#list');
 
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const item = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        
+        item.textContent = input.value;
+        deleteButton.textContent = 'X';
+        deleteButton.classList.add('delete');
+
+        item.append(deleteButton);
+        
+        list.append(item);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(item);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+    } else {
+        input.focus();
+    }
+});
