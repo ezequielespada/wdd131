1f8c90814d99c538221ff0bbaf7065901482b377
Ezequiel Espada <e.h.espada@gmail.com>
2024-09-28 21:02:18 -0300

    Final corrections

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,18 +1,15 @@
-// Función para el menú hamburguesa
 const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 const title = document.querySelector('h1');
 
-// Añadir evento click al botón hamburguesa para mostrar el menú y ocultar el h1
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
     
-    // Ocultar el h1 cuando se abre el menú
     if (mainnav.classList.contains('show')) {
-        title.style.opacity = '0'; // Oculta el título suavemente
+        title.style.opacity = '0'; 
     } else {
-        title.style.opacity = '1'; // Muestra el título suavemente
+        title.style.opacity = '1'; 
     }
 });
 
