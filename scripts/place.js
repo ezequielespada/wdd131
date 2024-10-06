586af371a8ab6b63496af13eac8ec29fea35147b
Ezequiel Espada <e.h.espada@gmail.com>
2024-10-06 02:59:06 -0300

    adjust in js

--- scripts/place.js
+++ scripts/place.js
@@ -1,7 +1,5 @@
-// Mostrar la última modificación
 document.getElementById("last-modified").textContent = document.lastModified;
 
-// Calcular sensación térmica
 function calculateWindChill(temperature, windSpeed) {
     if (temperature <= 10 && windSpeed > 4.8) {
         return (13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16)).toFixed(1) + " °C";
@@ -10,7 +8,6 @@ function calculateWindChill(temperature, windSpeed) {
     }
 }
 
-// Definir los valores estáticos para la sensación térmica
-const temp = 30; // Cambia esto si lo necesitas
-const windSpeed = 10; // Cambia esto si lo necesitas
+const temp = 30;
+const windSpeed = 10;
 document.getElementById("wind-chill").textContent = calculateWindChill(temp, windSpeed);
