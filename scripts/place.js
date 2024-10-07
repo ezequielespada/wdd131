8da2c1fe3a46454aca33fe7407587021891f65ad
Ezequiel Espada <e.h.espada@gmail.com>
2024-10-07 00:45:22 -0300

    minors corrections

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,8 @@
+// Footer
 document.getElementById("last-modified").textContent = document.lastModified;
+document.getElementById("current-year").textContent = new Date().getFullYear();
 
+// Wind chill function
 function calculateWindChill(temperature, windSpeed) {
     if (temperature <= 10 && windSpeed > 4.8) {
         return (13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16)).toFixed(1) + " °C";
@@ -8,6 +11,8 @@ function calculateWindChill(temperature, windSpeed) {
     }
 }
 
+// Variables
 const temp = 30;
 const windSpeed = 10;
+
 document.getElementById("wind-chill").textContent = calculateWindChill(temp, windSpeed);
